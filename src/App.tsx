97e14fc3ce60930b1e0216c29cfaf074ea85220a
ElioRivas <eliocarmen@hotmail.com>
2022-12-13 19:14:24 -0600

    only for commit

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ const App = () => {
   return ( 
     <div className='bg-primary w-full overflow-hidden'>
 
-      {/* Navbar */}
+      {/* Navbar color changes*/}
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
         <div className={`${styles.boxWidth}`}>
           <Navbar/>
